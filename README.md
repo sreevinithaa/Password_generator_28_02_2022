832ee0801e82f63880676f77570fac961b8d332a
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-02-27 16:10:15 +0800

    Add script functionality

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password_generator_28_02_2022
+# Password Generator
+
+## Description
+This is an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+## Table of content
+![The Password Generator application displays a red button to "Generate Password".](./assets/img/screentshot.png)
+
+## Repository
+
+You can get the code in the following link. https://github.com/sreevinithaa/Password_generator_28_02_2022.git
+
+## Production link
+
+You can view my portfolio through this live link https://sreevinithaa.github.io/Password_generator_28_02_2022/index.html
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,72 @@
-// Assignment Code
+
+
+var passwordlength = document.querySelector("#password_length");
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// assign charector set to constant variable
+const UpperCaseValue = "ABCDEFGHIJKLMNOPQUSTUVWXYZ";
+const LowerCaseValue = "abcdefghijklmnopqrstuvwxyz";
+const Numbers = "1234567890";
+const Symbols = "~!@#$%^&*()?";
 
-  passwordText.value = password;
+//Which will generate password based to cretieria.
+function generatePassword() {
+  var charset = "";
+  var password = "";
+  charset = GenerateCharectorSet();
+  if (isEmpty(charset)) {
+    alert("Please check atleast on option!");
+    return;
+  }
+  var length = passwordlength.value;
 
+  for (var i = 0; i < length; i++) {
+    var random_index = Math.floor(Math.random() * charset.length);
+
+    password = password + charset.charAt(random_index);
+  }
+
+  document.querySelector("#password").value = password;
 }
 
+//this method helps to check the value empty of null
+function isEmpty(value) {
+  return value == null || value.length === 0;
+}
+
+
+//Generate charector set based on selected criteria
+function GenerateCharectorSet() {
+  var charset = "";
+  var chkSymbol = document.querySelector("#chkSymbol");
+  var chknumber = document.querySelector("#chkNumber");
+  var chklower = document.querySelector("#chkLowercase");
+
+  var chkupper = document.querySelector("#chkUpperCase");
+  if (chkupper.checked) {
+    charset = charset + UpperCaseValue;
+  }
+  if (chklower.checked) {
+    charset = charset + LowerCaseValue;
+  }
+  if (chknumber.checked) {
+    charset = charset + Numbers;
+  }
+  if (chkSymbol.checked) {
+    charset = charset + Symbols;
+  }
+  return charset;
+}
+
+//This method helps to update the span when the user change the slider value
+function fuc_SlideValueChange() {
+  var length = passwordlength.value;
+  document.querySelector("#span_display_length").innerHTML = length;
+}
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+passwordlength.addEventListener("input", fuc_SlideValueChange);
+fuc_SlideValueChange();
